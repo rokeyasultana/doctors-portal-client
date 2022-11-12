b789741d901c437f63ce1fb51c8eb2d14d88f18d
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-12 21:00:17 +0600

    banner

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     const menuItems = <React.Fragment>
     <li><Link to="/">Home</Link></li>
-    <li><Link to="/appointment">Appointment</Link></li>
-    <li><Link to="/about">About</Link></li>
-    <li><Link to="/reviews">Reviews</Link></li>
+    <li><Link to="">Appointment</Link></li>
+    <li><Link to="">About</Link></li>
+    <li><Link to="">Reviews</Link></li>
     <li><Link to="/login">Login</Link></li>
 </React.Fragment>
     return (
