38157af2b0838706fc91990a5c3225d1bd2d5ea3
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-17 13:43:36 +0600

    private route

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider'
 const Navbar = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(err => console.log(err));
+    }
     const menuItems = <React.Fragment>
     <li><Link to="/">Home</Link></li>
     <li><Link to="/appointment">Appointment</Link></li>
     <li><Link to="">About</Link></li>
-    <li><Link to="">Reviews</Link></li>
-    <li><Link to="/login">Login</Link></li>
+   
+    {user?.uid ?
+            <>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+                <li><button onClick={handleLogOut}>Sign out</button></li>
+            </>
+            : <li><Link to="/login">Login</Link></li>}
 </React.Fragment>
     return (
         <div className="navbar bg-base-100 flex justify-between">
