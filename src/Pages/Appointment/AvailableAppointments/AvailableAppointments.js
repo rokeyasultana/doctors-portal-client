a9916cc129c19eddb874b0c3b8654f0efb64d07b
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-19 19:57:30 +0600

    booking slots]

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,18 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointments = ({ selectedDate }) => {
-    const [appointmentOptions, setAppointmentOptions] = useState([]);
+    // const [appointmentOptions, setAppointmentOptions] = useState([]);
+    
     const [treatment, setTreatment] = useState(null);
 
+    const date = format(selectedDate, 'PP');
+    const { data: appointmentOptions = [], refetch, isLoading } = useQuery({
+        queryKey: ['appointmentOptions', date],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/appointmentOptions?date=${date}`);
+            const data = await res.json();
+            return data
+        }
+    });
 
- useEffect(() => {
-        fetch('appointmentOptions.json')
-            .then(res => res.json())
-            .then(data => setAppointmentOptions(data))
-    }, [])
+   
+//  useEffect(() => {
+//         fetch('http://localhost:5000/appointment')
+//             .then(res => res.json())
+//             .then(data => setAppointmentOptions(data))
+//     }, [])
 
     return (
         <section  className='my-16'>
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,43 +1,67 @@
 import React from 'react';
 import { format } from 'date-fns';
+import { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import toast, { Toaster } from 'react-hot-toast';
 
 
 const BookingModal = ({ treatment,
-    setTreatment, selectedDate }) => {
-    const { name, slots } = treatment;
-    const date = format(selectedDate, 'PP');
-
-    const handleBooking = event => {
-        event.preventDefault();
-        const form = event.target;
-        const slot = form.slot.value;
-        const name = form.name.value;
-        const email = form.email.value;
-        const phone = form.phone.value;
-        
-        const booking = {
-            appointmentDate: date,
-            treatment: name,
-            patient: name,
-            slot,
-            email,
-            phone,
+    setTreatment, selectedDate, refetch }) => {
+        const { name: treatmentName, slots } = treatment;
+        const date = format(selectedDate, 'PP');
+        const { user } = useContext(AuthContext);
+    
+        const handleBooking = event => {
+            event.preventDefault();
+            const form = event.target;
+            const slot = form.slot.value;
+            const name = form.name.value;
+            const email = form.email.value;
+            const phone = form.phone.value;
+         
+            const booking = {
+                appointmentDate: date,
+                treatment: treatmentName,
+                patient: name,
+                slot,
+                email,
+                phone,
+            }
+    
+            // TODO: send data to the server
+            // and once data is saved then close the modal 
+            // and display success toast
+            fetch('http://localhost:5000/bookings', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(booking)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.acknowledged) {
+                        setTreatment(null);
+                        toast.success('Booking confirmed');
+                   refetch()
+                    }
+                    else{
+                        toast.error(data.message);
+                    }
+                })
+    
+    
         }
-
-        console.log(booking);
-        setTreatment(null);
-    }
-
 
     return (
 
         <>
-        
         <input type="checkbox" id="booking-modal" className="modal-toggle" />
         <div className="modal">
             <div className="modal-box relative">
                 <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                <h3 className="text-lg font-bold">{name}</h3>
+                <h3 className="text-lg font-bold">{treatmentName}</h3>
                 <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
                     <input type="text" disabled value={date} className="input w-full input-bordered " />
                     <select name="slot" className="select select-bordered w-full">
@@ -48,16 +72,19 @@ const BookingModal = ({ treatment,
                             >{slot}</option>)
                         }
                     </select>
-                    <input name="name" type="text" placeholder="Your Name" className="input w-full input-bordered" />
-                    <input name="email" type="email" placeholder="Email Address" className="input w-full input-bordered" />
+                    <input name="name" type="text" defaultValue={user?.displayName} disabled placeholder="Your Name" className="input w-full input-bordered" />
+                    <input name="email" type="email" defaultValue={user?.email} disabled placeholder="Email Address" className="input w-full input-bordered" />
                     <input name="phone" type="text" placeholder="Phone Number" className="input w-full input-bordered" />
                     <br />
                     <input className='btn btn-accent w-full' type="submit" value="Submit" />
                 </form>
             </div>
         </div>
+        <Toaster/>
     </>
-    );
+);
 };
 
+
+  
 export default BookingModal;
