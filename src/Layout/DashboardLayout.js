f096cee404dbd34982a02417065abdb56127b187
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-22 13:50:27 +0600

    admin

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider/AuthProvider';
+import useAdmin from '../hooks/useAdmin';
 import Navbar from '../Pages/Shared/Navbar/Navbar'
 const DashboardLayout = () => {
+const {user} = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user.email);
+
+
+
     return (
         <div>
         <Navbar></Navbar>
@@ -14,9 +21,14 @@ const DashboardLayout = () => {
                 <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 bg-base-100 text-base-content">
                     <li><Link to="/dashboard">My Appointments</Link></li>
-                
-                           
-                       
+                   { 
+                   isAdmin && <>
+                   
+                   <li><Link to="/dashboard/allusers">All Users</Link></li>
+                   </>
+                   
+                   }
+            
 
                 </ul>
 
